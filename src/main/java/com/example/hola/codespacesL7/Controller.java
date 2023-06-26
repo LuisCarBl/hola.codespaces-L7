19de501625e4f5bb97a9c5ff3a211ec91b76a6aa
LuisCarBl <134304449+LuisCarBl@users.noreply.github.com>
2023-06-26 15:03:26 +0000

    esto va para render

---
+++ src/main/java/com/example/hola/codespacesL7/Controller.java
@@ -0,0 +1,16 @@
+package com.example.hola.codespacesL7;
+
+import java.lang.String;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping(path="/")
+public class Controller {
+    
+    @GetMapping(path="/")
+    public String home() { 
+        return "LN90161994 - Luis Carmen Blas";
+    }
+}
---
+++ src/main/java/com/example/hola/codespacesL7/HolaCodepacesL17Application.java
@@ -0,0 +1,13 @@
+package com.example.hola.codespacesL7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaCodepacesL17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaCodepacesL17Application.class, args);
+	}
+
+}
